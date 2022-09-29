8a77738132e0ad5917bdf3ba8c758843a741e654
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-09-30 06:29:51 +0700

    tes

--- games/mamin_commands.php
+++ games/mamin_commands.php
@@ -54,6 +54,7 @@ elseif($subcommand == "play"){
             'starting_check',
             time()+5,
         ];
+        server_start();
     }
 }
 elseif($subcommand == "extend" and !empty($data['playing_chatters'][$chat_id]['mamin']['step'])
