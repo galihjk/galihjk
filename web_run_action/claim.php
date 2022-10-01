ab9339c02f94eba24bfbc1aac9453daa80dd45f4
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-10-02 01:45:02 +0700

    tokped

--- web_run_action/claim.php
+++ web_run_action/claim.php
@@ -23,23 +23,31 @@ if(empty($userdata['unclaimeds'][$gametype][$claim_chat_id][$claimcode])){
 
 $unclaimeds = $userdata['unclaimeds'];
 $claimdata = $unclaimeds[$gametype][$claim_chat_id][$claimcode];
+$poin = $claimdata[0];
+
+unset($unclaimeds[$gametype][$claim_chat_id][$claimcode]);
+
+setUser($user_id,['unclaimeds'=>$unclaimeds]);
 
 KirimPerintah('sendMessage',[
     'chat_id' => $user_id,
-    'text'=> "Selamat, user. Anda telah mendapatkan xxx poin." . print_r($claimdata, true),
+    'text'=> "Selamat. Anda telah mendapatkan $poin poin.",
     'parse_mode'=>'HTML',
 ]);
 
 $tokpeds = explode(" ",loadData("setting/tokpeds"));
 $tokpeds_index = loadData("setting/tokpeds_index",0);
+
+if(!isset($tokpeds[$tokpeds_index])){
+    $tokpeds_index = 0;
+    shuffle($tokpeds);
+    saveData("setting/tokpeds",implode(" ",$tokpeds));   
+}
+
+$tokped_code = $tokpeds[$tokpeds_index];
+
 $tokpeds_index++;
 saveData("setting/tokpeds_index",$tokpeds_index);
-
-KirimPerintah('sendMessage',[
-    'chat_id' => $user_id,
-    'text'=> "Tokepes: $tokpeds_index.." . print_r($tokpeds, true),
-    'parse_mode'=>'HTML',
-]);
 
 ?>
     Berhasil, silakan close browser ini.
@@ -48,7 +56,7 @@ KirimPerintah('sendMessage',[
         if(!sudah){
             sudah = true;
             setTimeout(() => {
-                window.location.href = "https://tokopedia.link/kZ8Br26bosb";
+                window.location.href = "https://tokopedia.link/<?= $tokped_code ?>";
             }, 500);
         }
     </script>
@@ -60,8 +68,10 @@ skip_to_end:
     <script>
         setTimeout(() => {
             window.close('','_parent','');
+        }, 1000);
+        setTimeout(() => {
             window.location.href = "https://t.me/<?= $config['bot_username'] ?>";
-        }, 500);
+        }, 1500);
     </script>
 <?php
 
