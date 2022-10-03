29e5302e779d0aae542cf3cb2ca632065016b641
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-10-04 06:28:40 +0700

    soal_tambah

--- games/soal_callback_query.php
+++ games/soal_callback_query.php
@@ -1,27 +1,46 @@
 <?php
 $callback_query_data = $update["callback_query"]['data'];
+$chat_id = (string) $update["callback_query"]["message"]["chat"]["id"];
+$from_id = $update["callback_query"]['from']['id'];
+
 if($callback_query_data == "soal_survey_tambah"){
     KirimPerintah('answerCallbackQuery',[
         'callback_query_id' => $update["callback_query"]['id'],
-        'text'=> "soal_survey_tambah dipilih",
-    ]);	
+        'text'=> "soal_survey_tambah $emoji_like",
+    ]);
+    KirimPerintah('sendMessage',[
+        'chat_id' => $chat_id,
+        'text'=> "Balas pesan ini untuk membuat soal survey. \n".mentionUser($from_id),
+        'parse_mode'=>'HTML',
+        'reply_markup' => [
+            'force_reply'=>true,
+            'input_field_placeholder'=>'Tulis soal yang mau ditambahkan',
+            'selective'=>true,
+        ],
+    ]);
 }
 elseif($callback_query_data == "soal_survey_info"){
     KirimPerintah('answerCallbackQuery',[
         'callback_query_id' => $update["callback_query"]['id'],
-        'text'=> "soal_survey_info dipilih",
-    ]);	
-}
-elseif($callback_query_data == "soal_kuis_tambah"){
-    KirimPerintah('answerCallbackQuery',[
-        'callback_query_id' => $update["callback_query"]['id'],
-        'text'=> "soal_kuis_tambah dipilih",
+        'text'=> $emoji_like,
     ]);	
 }
 elseif($callback_query_data == "soal_kuis_info"){
     KirimPerintah('answerCallbackQuery',[
         'callback_query_id' => $update["callback_query"]['id'],
-        'text'=> "soal_kuis_info dipilih",
+        'text'=> $emoji_like,
+    ]);	
+}
+elseif($callback_query_data == "soal_komkat_info"){
+    KirimPerintah('answerCallbackQuery',[
+        'callback_query_id' => $update["callback_query"]['id'],
+        'text'=> $emoji_like,
+    ]);	
+}
+elseif($callback_query_data == "soal_huruf_info"){
+    KirimPerintah('answerCallbackQuery',[
+        'callback_query_id' => $update["callback_query"]['id'],
+        'text'=> $emoji_like,
     ]);	
 }
 else{
--- games/soal_commands.php
+++ games/soal_commands.php
@@ -1,10 +1,14 @@
 <?php 
-if($command == 'tambah_soal'){
+if($command == 'soal_tambah'){
     $reply_markup = inlineKeyBoard([
         ['survey', 'soal_survey_tambah'],
         ['?', 'soal_survey_info'],
-        ['kuis', 'soal_kuis_tambah'],
+        ['kuis*', 'underconstruction'],
         ['?', 'soal_kuis_info'],
+        ['komkat*', 'underconstruction'],
+        ['?*', 'soal_kuis_info'],
+        ['huruf*', 'underconstruction'],
+        ['?*', 'soal_kuis_info'],
     ],2);
     
     //kirim pesan dengan tombol
@@ -15,22 +19,4 @@ if($command == 'tambah_soal'){
         'reply_markup' => $reply_markup,
     ]);
     
-    // KirimPerintah('sendMessage',[
-    //     'chat_id' => $chat_id,
-    //     'text'=> "Balas pesan ini untuk membuat soal",
-    //     'parse_mode'=>'HTML',
-    //     'reply_markup' => [
-    //         'force_reply'=>true,
-    //         'input_field_placeholder'=>'Tulis soal yang mau ditambahkan',
-    //         'selective'=>true,
-    //     ],
-    // ]);
-}
-else{
-    KirimPerintah('sendMessage',[
-        'chat_id' => $chat_id,
-        'text'=> "command: $command",
-        'parse_mode'=>'HTML',
-        'reply_markup' => $reply_markup,
-    ]);
 }
