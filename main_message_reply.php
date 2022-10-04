ff3f7c64f1b2f1e693942df67484729b7fc23604
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-10-04 13:15:25 +0700

    fixing2 reply

--- main_message_reply.php
+++ main_message_reply.php
@@ -3,5 +3,5 @@ $reply_to_message = $message_data['reply_to_message'];
 $reply_to_message_text = $reply_to_message['text'] ?? '';
 if(isDiawali($reply_to_message_text,"[")){
     $game = strtolower(str_replace("[","",explode("]",$reply_to_message_text)[0])) ;
-    include ("galihjk/$game".'_reply.php');
+    include ("galihjk/games/$game".'_reply.php');
 }
