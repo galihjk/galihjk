296d16d28c462a11cb0bd97ceed1cf18429b04f4
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2022-11-10 10:39:39 +0700

    aduh lieur

--- bot.php
+++ bot.php
@@ -29,28 +29,8 @@ if(!empty($data)){
 		}
 	*/
 
-	$last_serve_time = intval(loadData("last_serve_time") ?? time());
-	if(abs($last_serve_time-time()) > 1){
-		$jeda = time() - intval($last_serve_time);
-		$data_playing_chatters = loadData("data_playing_chatters");
-		if(!empty($data_playing_chatters)){
-			include('galihjk/main.php');
-			if(empty($data_playing_chatters)){
-				file_put_contents("ERROR data_playing_chatters NOT LOADED 4.txt","data_playing_chatters DATA NOT LOADED 4.txt");
-			}
-			else{
-				saveData("data_playing_chatters",$data_playing_chatters);
-			}
-		}
-		else{
-			file_put_contents("ERROR data_playing_chatters NOT LOADED3.txt","data_playing_chatters DATA NOT LOADED3.txt");
-		}
-		
-	}
-	saveData("last_serve_time",time());
-
 	$update = json_decode(file_get_contents("php://input"), TRUE);
-	
+		
 	if(!empty($update)) {
 		$data_playing_chatters = loadData("data_playing_chatters");
 		if(!empty($data_playing_chatters)){
@@ -66,7 +46,33 @@ if(!empty($data)){
 			file_put_contents("ERROR data_playing_chatters NOT LOADED.txt","data_playing_chatters DATA NOT LOADED.txt");
 		}
 	}
-	
+	else{
+		$data_last_serve_time = loadData("last_serve_time");
+		if(empty($data_last_serve_time)){
+			file_put_contents("ERROR data_last_serve_time NOT LOADED x.txt","data_playing_chatters DATA NOT LOADED.txt");
+		}
+		else{
+			$last_serve_time = intval($data_last_serve_time);
+			if(abs($last_serve_time-time()) > 1){
+				$jeda = time() - intval($last_serve_time);
+				$data_playing_chatters = loadData("data_playing_chatters");
+				if(!empty($data_playing_chatters)){
+					include('galihjk/main.php');
+					if(empty($data_playing_chatters)){
+						file_put_contents("ERROR data_playing_chatters NOT LOADED 4.txt","data_playing_chatters DATA NOT LOADED 4.txt");
+					}
+					else{
+						saveData("data_playing_chatters",$data_playing_chatters);
+					}
+				}
+				else{
+					file_put_contents("ERROR data_playing_chatters NOT LOADED3.txt","data_playing_chatters DATA NOT LOADED3.txt");
+				}
+				
+			}
+			saveData("last_serve_time",time());
+		}
+	}
 	echo "<pre>";
 	print_r($data);
 	saveData("data", $data);
--- main.php
+++ main.php
@@ -50,22 +50,24 @@ if(!empty($data['delayedPerintah'])){
 	}
 }
 
-//change game step
-// 0 'game', 1 $chat_id, 2 'step', 3 time()+xx
 if(!empty($data['change_step'])){
-	foreach($data['change_step'] as $key=>$val){
-		if(!isset($val[3]) or time() >= $val[3]){
-			if(!empty($data_playing_chatters[$val[1]]['playing']) and $data_playing_chatters[$val[1]]['playing'] == $val[0]){
-				$data_playing_chatters[$val[1]][$val[0]]['step'] = $val[2];
+	//change game step
+	// 0 'game', 1 $chat_id, 2 'step', 3 time()+xx
+	if(!empty($data_playing_chatters)){
+		foreach($data['change_step'] as $key=>$val){
+			if(!isset($val[3]) or time() >= $val[3]){
+				if(!empty($data_playing_chatters[$val[1]]['playing']) and $data_playing_chatters[$val[1]]['playing'] == $val[0]){
+					$data_playing_chatters[$val[1]][$val[0]]['step'] = $val[2];
+				}
+				else{
+					file_put_contents("gjlog/error_change_step".date("YmdHis").".txt",print_r([$data_playing_chatters,$val],1));
+				}
+				unset($data['change_step'][$key]);
 			}
-			else{
-				file_put_contents("gjlog/error_change_step".date("YmdHis").".txt",print_r([$data_playing_chatters,$val],1));
-			}
-			unset($data['change_step'][$key]);
 		}
+		saveData("data_playing_chatters",$data_playing_chatters);
 	}
-}
-
-if(!empty($data_playing_chatters)){
-	saveData("data_playing_chatters",$data_playing_chatters);
+	else{
+		file_put_contents("data_playing_chatters_kosong2.txt","au ah!2");
+	}
 }
